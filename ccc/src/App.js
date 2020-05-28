c22cd29bb76a2623a9dc4cbc0f19d94211b30553
Austin Abbott <magicaustin51547@gmail.com>
2020-05-27 20:47:29 -0600

    Add popper and jquery

--- ccc/src/App.js
+++ ccc/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "../src/components/header.jsx";
 import Footer from "../src/components/footer.jsx";
+import Home from "../src/pages/home.jsx"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
